bde7369e7728a65904bf6b13d96e43d9dbbe1a20
Aleksanyan <karenaleksanyan1984@gmail.com>
2019-03-14 21:14:50 +0400

    hello

---
+++ app/src/main/java/com/example/aleksanyan/a1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.aleksanyan.a1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button b;
+    TextView t;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        t.setText("Hello Android ");
+    }
+}
